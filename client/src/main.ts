3dd5c134acae529c4902bdaf548a5d8549e1e938
l0snight <rainbowzhang00@gmail.com>
2021-08-03 18:28:16 +0800

    Add: Config router

--- client/src/main.ts
+++ client/src/main.ts
@@ -1,9 +1,11 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 import ElementPlus from 'element-plus'
+import router from './router'
 import 'element-plus/lib/theme-chalk/index.css'
 import './index.css'
 
 createApp(App)
   .use(ElementPlus)
+  .use(router)
   .mount('#app')
